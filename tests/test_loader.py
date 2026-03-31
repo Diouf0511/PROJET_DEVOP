c4a88cff7eece609cd66738db0577950162a8433
Seynabou Diouf <dioufseynabou2021@gmail.com>
2026-03-31 01:17:28 +0100

    first commit

---
+++ tests/test_loader.py
@@ -0,0 +1,14 @@
+from data.loader import load_data
+
+def test_data_not_empty():
+    df = load_data()
+    assert not df.empty
+
+def test_columns_exist():
+    df = load_data()
+    assert 'age' in df.columns
+    assert 'sex' in df.columns
+
+def test_no_null_age():
+    df = load_data()
+    assert df['age'].isnull().sum() == 0
